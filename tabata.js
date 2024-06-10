2ad07b5cf3f47aae3314200b88cda5e94d2b0048
disharmonia666 <disharmonia666@gmail.com>
2024-06-10 13:16:19 +0200

    feat/added voice

--- tabata.js
+++ tabata.js
@@ -1,26 +1,59 @@
 (async () => {
   const inquirer = await import('inquirer');
+  const say = require('say');
+  const readline = require('readline');
 
   function playSound() {
     process.stdout.write('\u0007');
   }
 
+  // Function to speak text
+  function speakText(text) {
+    // Speak the text
+    say.speak(text, 'Shelley', 1.0, (err) => {
+      if (err) {
+        console.error(`Error: ${err.message}`);
+        return;
+      }
+    });
+  }
+
   function tabata(exercise, workTime, readyTime) {
     return new Promise((resolve) => {
+      speakText(exercise);
       const intervalId = setInterval(() => {
+        let message = '';
         if (readyTime < 0) {
-          process.stdout.write(`\r${exercise} (${workTime} seconds remaining)`);
-          if (workTime <= 3) {
-            playSound();
+          //process.stdout.write(`\r${exercise} (${workTime} seconds remaining)`);
+          message = `${exercise} `;
+          if (workTime === 0) {
+            playSound()
+          } else {
+            message = message + `(${workTime} seconds remaining)`
+            if (workTime <= 3) {
+              speakText(workTime);
+            }
           }
           workTime--;         
         } else {
-          process.stdout.write(`\rReady to ${exercise} (${readyTime} seconds remaining)`);
-          if (readyTime <= 3) {
-            playSound();
+          //process.stdout.write(`\rGet ready for the ${exercise} (${readyTime} seconds remaining)`);
+          message = `${exercise}. Get ready `;
+          if (readyTime === 0) {
+            message = message + `start!`
+            playSound()
+          } else {
+            message = message + `(${readyTime} seconds remaining)`
+            if (readyTime <= 3) {
+              speakText(readyTime);
+            }
           }
           readyTime--;
         }
+
+        // Clear the line and write the new message
+        readline.clearLine(process.stdout, 0);
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write(message);
 
         if (workTime === -1 && readyTime === -1) {
           clearInterval(intervalId);
@@ -32,7 +65,7 @@
   }
 
   async function runJointWarmUp() {
-    await tabata("Swing your arms forward", 10, 0);
+    await tabata("Swing your arms forward", 10, 10);
     await tabata("Swing your arms back", 10, 0);
     await tabata("Head tilts", 10, 0);
     await tabata("Head nods", 10, 0);
@@ -62,14 +95,14 @@
   }
 
   async function runBackYoga() {
-    await tabata("Prepare", 5, 0);
-    await tabata("Child pose", 30, 5);
+    await tabata("Child pose", 30, 10);
     await tabata("Cat cow", 30, 5);
     await tabata("Standing forward fold", 30, 5);
     await tabata("Yogi squat", 30, 5);
     await tabata("Seated twist right", 30, 5);
     await tabata("Seated twist left", 30, 5);
     await tabata("Knee hug", 30, 5);
+    await tabata("Easy twist", 30, 5);
     await tabata("Fullbody stretch", 30, 5);
     await tabata("Lying twist right", 45, 5);
     await tabata("Lying twist left", 45, 5);
@@ -78,7 +111,7 @@
 
   async function runTest() {
     await tabata("Child pose", 10, 5);
-    await tabata("Cat cow", 10, 5);
+    await tabata("Cat cow", 10, 0);
   }
 
   async function selectWorkout() {
@@ -109,5 +142,6 @@
     }
   }
 
+  
   selectWorkout();
 })();
